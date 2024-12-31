8d96c0983d98e94f80a84d998923be6ce4818522
Fadhel Akbar <fadhelakbarsallang@gmail.com>
2024-12-31 16:47:01 +0800

    test hapus configurasi build vite

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
-    @vite(['resources/css/app.css','resources/js/app.js'])
+    @vite(['resources/css/app.css','resources/js/app.js'], 'build/vite')
 </head>
 <body>
     <nav class="bg-white border-gray-200 dark:border-gray-600 dark:bg-gray-900">
