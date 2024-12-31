6af5c2ba4e451abd93d98369290a71228316f2e2
Fadhel Akbar <fadhelakbarsallang@gmail.com>
2025-01-01 01:13:32 +0800

    test post data

--- app/Http/Controllers/PublishOrderController.php
+++ app/Http/Controllers/PublishOrderController.php
@@ -11,14 +11,8 @@ class PublishOrderController extends Controller
     {
         $request->validate([
             'name' => 'required|string|max:255',
-            'book_title' => 'required|string|max:255',
             'email' => 'required|email|max:255',
             'phone' => 'required|string|max:20',
-            'date_of_birth' => 'required',
-            'gender' => 'required|in:pria,wanita',
-            'institution' => 'required|string|max:255',
-            'job_title' => 'required|string|max:255',
-            // 'pdf_path' => 'required|file|mimes:pdf,doc,docx|max:10240',
             'service_type' => 'required|in:penerbitan_buku,mencetak_buku,menerbitkan_dan_mencetak',
             'book_size' => 'nullable|in:100-250 halaman,250-550 halaman',
             'print_quantity' => 'nullable|integer|min:1',
@@ -28,14 +22,8 @@ class PublishOrderController extends Controller
         // Simpan data ke database
         ServiceOrder::create([
             'name' => $request->input('name'),
-            'book_title' => $request->input('book_title'),
             'email' => $request->input('email'),
             'phone' => $request->input('phone'),
-            'date_of_birth' => $request->input('date_of_birth'),
-            'gender' => $request->input('gender'),
-            'institution' => $request->input('institution'),
-            'job_title' => $request->input('job_title'),
-            // 'pdf_path' => $request->input('pdf_path'),
             'service_type' => $request->input('service_type'),
             'book_size' => $request->input('book_size'),
             'print_quantity' => $request->input('print_quantity'),
