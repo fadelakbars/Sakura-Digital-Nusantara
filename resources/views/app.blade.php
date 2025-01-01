3bbfc70a997199c9cb0fe9208f518511a6e43030
Fadhel Akbar <fadhelakbarsallang@gmail.com>
2025-01-01 18:14:03 +0800

    update wa link

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -108,7 +108,7 @@
     </div>
     
     <div data-dial-init class="fixed bottom-6 end-24 group">
-        <a href="https://wa.me/628114101349?text=saya%20ingin%20menerbitkan%20buku%20di%20sakura">
+        <a href="https://wa.me/628114101349?text=halo%saya%20ingin%20menerbitkan%20buku%20di%20sakura">
             <button type="button" data-dial-toggle="speed-dial-menu-text-outside-button-square" aria-controls="speed-dial-menu-text-outside-button-square" aria-expanded="false" class="flex items-center justify-center text-white bg-lime-500 rounded-full w-14 h-14 hover:bg-green-600 dark:bg-blue-600 dark:hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
@@ -178,7 +178,7 @@
                             <a href={{ url('url', []) }} class="hover:underline">publishersakura@gmail.com</a>
                         </li>
                         <li class="mb-4">
-                            <a href={{ url('https://wa.me/628114101349?text=saya%20ingin%20menerbitkan%20buku%20di%20sakura', []) }} class="hover:underline">+628114101349</a>
+                            <a href={{ url('https://wa.me/628114101349?text=halo%saya%20ingin%20menerbitkan%20buku%20di%20sakura', []) }} class="hover:underline">+628114101349</a>
                         </li>
                         <li class="mb-4">
                             <a href={{ url('url', []) }} class="hover:underline">Jl. Biola Raya Blok 10, Makassar</a>
