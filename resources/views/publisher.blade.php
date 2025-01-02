8aa92884bb374ed25ab4124d4d13c782ef0cf493
Fadhel Akbar <fadhelakbarsallang@gmail.com>
2025-01-03 04:52:48 +0800

    menambahkan link pada card buku

--- resources/views/publisher.blade.php
+++ resources/views/publisher.blade.php
@@ -497,7 +497,7 @@
                             </a>
                             <div class="flex items-center justify-between">
                                 <span class="text-3xl font-bold text-gray-900 dark:text-white">Rp. {{ $book->price }}</span>
-                                <a href={{ $book->link }} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+                                <a href="{{ $book->link }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
                             </div>
                         </div>
                     </div>
