04e5251bffc2f53635db930040d4210f4e03684b
Fadhel Akbar <fadhelakbarsallang@gmail.com>
2025-01-03 05:47:23 +0800

    add view profil and oraganization structure

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -5,67 +5,62 @@
 @section('content')
 
 <section  class="bg-white dark:bg-gray-900">
-    <div class="flex flex-col items-center">
-        <!-- Root Node -->
-        <div class="flex items-center">
-            <div class="bg-blue-500 text-white font-bold py-2 px-4 rounded shadow-lg">
-                CV. SDN
-            </div>
-        </div>
-    
-        <!-- Level 1 -->
-        <div class="flex flex-col items-center mt-4">
-            <div class="w-0 h-4 border-l-2 border-gray-300"></div>
-            <div class="bg-gray-200 text-gray-700 font-medium py-2 px-4 rounded shadow-lg">
-                Direktur
-            </div>
-        </div>
-    
-        <!-- Level 2 -->
-        <div class="flex justify-center items-center mt-4">
-            <div class="relative flex flex-col items-center mx-8">
+    <div class="p-10 mb-40">
+        <div class="flex flex-col items-center">
+        
+            <!-- Level 1 -->
+            <div class="flex flex-col items-center mt-4">
                 <div class="w-0 h-4 border-l-2 border-gray-300"></div>
                 <div class="bg-gray-200 text-gray-700 font-medium py-2 px-4 rounded shadow-lg">
-                    Komisaris
+                    Direktur
                 </div>
             </div>
-            <div class="relative flex flex-col items-center mx-8">
-                <div class="w-0 h-4 border-l-2 border-gray-300"></div>
-                <div class="bg-gray-200 text-gray-700 font-medium py-2 px-4 rounded shadow-lg">
-                    CEO
+        
+            <!-- Level 2 -->
+            <div class="flex justify-center items-center mt-4">
+                <div class="relative flex flex-col items-center mx-8">
+                    <div class="w-0 h-4 border-l-2 border-gray-300"></div>
+                    <div class="bg-gray-200 text-gray-700 font-medium py-2 px-4 rounded shadow-lg">
+                        Komisaris
+                    </div>
+                </div>
+                <div class="relative flex flex-col items-center mx-8">
+                    <div class="w-0 h-4 border-l-2 border-gray-300"></div>
+                    <div class="bg-gray-200 text-gray-700 font-medium py-2 px-4 rounded shadow-lg">
+                        CEO
+                    </div>
                 </div>
             </div>
-        </div>
-    
-        <!-- Level 3 -->
-        <div class="flex justify-center items-center mt-4">
-            <div class="flex flex-col items-center mx-4">
-                <div class="w-0 h-4 border-l-2 border-gray-300"></div>
-                <div class="bg-gray-100 text-gray-500 font-medium py-2 px-4 rounded shadow">
-                    Divisi Publisher
+        
+            <!-- Level 3 -->
+            <div class="flex justify-center items-center mt-4">
+                <div class="flex flex-col items-center mx-4">
+                    <div class="w-0 h-4 border-l-2 border-gray-300"></div>
+                    <div class="bg-gray-100 text-gray-500 font-medium py-2 px-4 rounded shadow">
+                        Divisi Publisher
+                    </div>
                 </div>
-            </div>
-            <div class="flex flex-col items-center mx-4">
-                <div class="w-0 h-4 border-l-2 border-gray-300"></div>
-                <div class="bg-gray-100 text-gray-500 font-medium py-2 px-4 rounded shadow">
-                    Divisi Journal
+                <div class="flex flex-col items-center mx-4">
+                    <div class="w-0 h-4 border-l-2 border-gray-300"></div>
+                    <div class="bg-gray-100 text-gray-500 font-medium py-2 px-4 rounded shadow">
+                        Divisi Journal
+                    </div>
                 </div>
-            </div>
-            <div class="flex flex-col items-center mx-4">
-                <div class="w-0 h-4 border-l-2 border-gray-300"></div>
-                <div class="bg-gray-100 text-gray-500 font-medium py-2 px-4 rounded shadow">
-                    Application & Data Center
+                <div class="flex flex-col items-center mx-4">
+                    <div class="w-0 h-4 border-l-2 border-gray-300"></div>
+                    <div class="bg-gray-100 text-gray-500 font-medium py-2 px-4 rounded shadow">
+                        Application & Data Center
+                    </div>
                 </div>
-            </div>
-            <div class="flex flex-col items-center mx-4">
-                <div class="w-0 h-4 border-l-2 border-gray-300"></div>
-                <div class="bg-gray-100 text-gray-500 font-medium py-2 px-4 rounded shadow">
-                    IT Development
+                <div class="flex flex-col items-center mx-4">
+                    <div class="w-0 h-4 border-l-2 border-gray-300"></div>
+                    <div class="bg-gray-100 text-gray-500 font-medium py-2 px-4 rounded shadow">
+                        IT Development
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    
 </section>
 
 @endsection
