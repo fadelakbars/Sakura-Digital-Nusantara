54bea89993dbf46d77438e649b84fb7a26d7b26e
Fadhel Akbar <fadhelakbarsallang@gmail.com>
2025-01-04 00:58:53 +0800

    update view detail buku dan book resource menambahkan kolom link tokopedia

--- resources/views/detail-buku.blade.php
+++ resources/views/detail-buku.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <section class="bg-white dark:bg-gray-900 py-12">
     <div class="container mx-auto px-4">
-        <div class="flex flex-wrap justify-center gap-10">
+        <div class="grid grid-cols-2 md:grid-cols-3 gap-2">
             <!-- Book Cover -->
             <div class="flex justify-center items-center">
                 <img src="{{ asset('storage/' . $book->cover_image) }}" alt="{{ $book->title }}" class="rounded-lg shadow-lg w-64 h-96 object-cover">
@@ -17,10 +17,6 @@
                 <table class="table-auto text-gray-700 dark:text-gray-400">
                     <tbody>
                         <tr>
-                            <td class="font-semibold pr-4">Deskripsi</td>
-                            <td>:{{ $book->description }}</td>
-                        </tr>
-                        <tr>
                             <td class="font-semibold pr-4">Penulis</td>
                             <td>:{{ $book->author }}</td>
                         </tr>
@@ -30,7 +26,7 @@
                         </tr>
                         <tr>
                             <td class="font-semibold pr-4">Tahun Terbit</td>
-                            <td>:{{ $book->published_year }}</td>
+                            <td>:{{ $book->year }}</td>
                         </tr>
                         <tr>
                             <td class="font-semibold pr-4">Kategori</td>
@@ -42,20 +38,20 @@
                         </tr>
                         <tr>
                             <td class="font-semibold pr-4">Stok</td>
-                            <td>:{{ $book->stock }}</td>
+                            <td>:{{ $book->stock > 0 ? 'Tersedia' : 'Habis' }}</td>
                         </tr>
                     </tbody>
                 </table>
 
                 <div class="items-center mt-10 justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4 rtl:space-x-reverse">
-                    <a href={{ $book->link }} class="w-full sm:w-auto bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
+                    <a href={{ url('https://shopee.co.id/sakura_publisher', []) }} class="w-full sm:w-auto bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
                         <img src={{ asset('img/shopee.png') }} alt="Tokopedia Mascot" class="me-3 w-10 h-10">
                         <div class="text-left rtl:text-right">
                             <div class="mb-1 text-xs">Belanja di</div>
                             <div class="-mt-1 font-sans text-sm font-semibold">Shopee</div>
                         </div>
                     </a>
-                    <a href={{ $book->tokopedia }} class="w-full sm:w-auto bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
+                    <a href={{ url('https://www.tokopedia.com/sakurapublisher', []) }} class="w-full sm:w-auto bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
                         <img src={{ asset('img/Tokopedia_Mascot.png') }} alt="Tokopedia Mascot" class="me-3 w-10 h-10">
                         <div class="text-left rtl:text-right">
                             <div class="mb-1 text-xs">Belanja di</div>
