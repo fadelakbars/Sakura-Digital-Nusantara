27d8efd1d0dd2ea0060465d4383cbfe7da2af896
Fadhel Akbar <fadhelakbarsallang@gmail.com>
2025-01-04 01:40:44 +0800

    fix layout detail buku

--- resources/views/detail-buku.blade.php
+++ resources/views/detail-buku.blade.php
@@ -17,6 +17,10 @@
                 <table class="table-auto text-gray-700 dark:text-gray-400 w-full">
                     <tbody>
                         <tr>
+                            <td class="font-semibold pr-4">Deskripsi</td>
+                            <td>: {{ $book->description }}</td>
+                        </tr>
+                        <tr>
                             <td class="font-semibold pr-4">Penulis</td>
                             <td>: {{ $book->author }}</td>
                         </tr>
@@ -47,14 +51,14 @@
                     <a href={{ $book->link }} class="w-full sm:w-auto bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
                         <img src={{ asset('img/shopee.png') }} alt="Tokopedia Mascot" class="me-3 w-10 h-10">
                         <div class="text-left rtl:text-right">
-                            <div class="mb-1 text-xs">Belanja di</div>
+                            <div class="mb-1 text-xs">Dapatkan di</div>
                             <div class="-mt-1 font-sans text-sm font-semibold">Shopee</div>
                         </div>
                     </a>
-                    <a href={{ $book->tokopedia }} class="w-full sm:w-auto bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
+                    <a href={{ $book->link }} class="w-full sm:w-auto bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
                         <img src={{ asset('img/Tokopedia_Mascot.png') }} alt="Tokopedia Mascot" class="me-3 w-10 h-10">
                         <div class="text-left rtl:text-right">
-                            <div class="mb-1 text-xs">Belanja di</div>
+                            <div class="mb-1 text-xs">Dapatkan di</div>
                             <div class="-mt-1 font-sans text-sm font-semibold">Tokopedia</div>
                         </div>
                     </a>
