b22e3c7cc16fc943511dc139de6ac0be5214f72a
Fadhel Akbar <fadhelakbarsallang@gmail.com>
2025-01-04 07:51:46 +0800

    menambahkan tabel daftar jurnal pada halaman jurnal

--- resources/views/journal.blade.php
+++ resources/views/journal.blade.php
@@ -20,8 +20,8 @@
 
 {{-- tabel daftar jurnal --}}
 <section class="bg-white dark:bg-gray-900">
-    <div class="px-20 justify-center">
-
+    <div class="p-20 justify-center">
+        
         <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
             <div class="flex items-center justify-between flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4 bg-white dark:bg-gray-900">
                 <label for="table-search" class="sr-only">Search</label>
