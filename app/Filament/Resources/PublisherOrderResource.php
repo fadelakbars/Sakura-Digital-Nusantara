3f92955656add3d64ce9d59e0cfd229f6e2bd071
Fadhel Akbar <fadhelakbarsallang@gmail.com>
2025-01-06 04:44:30 +0800

    debugging eror relasi di publish order resource

--- app/Filament/Resources/PublisherOrderResource.php
+++ app/Filament/Resources/PublisherOrderResource.php
@@ -60,7 +60,7 @@ class PublisherOrderResource extends Resource
                     ->label('Paket Penerbitan'),
 
                 Select::make('print_quantity_id')
-                    ->relationship('printQuantity', 'quantity')
+                    ->relationship('printQuantity', 'price_per_unit')
                     ->label('Harga Cetakan Perbuku'),
                 // END RELASI TABEL
 
