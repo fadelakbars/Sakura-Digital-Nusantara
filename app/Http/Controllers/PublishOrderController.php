3e125ef09fe8b7eae2f2af24be267964fad435f4
Fadhel Akbar <fadhelakbarsallang@gmail.com>
2025-01-07 02:12:03 +0800

    update form order buku

--- app/Http/Controllers/PublishOrderController.php
+++ app/Http/Controllers/PublishOrderController.php
@@ -26,35 +26,19 @@ class PublishOrderController extends Controller
             'client_birthdate' => 'required|date|before:today',
             'client_job_title' => 'required|string|max:20',
             'client_institution' => 'required|string|max:20',
-            'manuscript_path' => 'required|string|max:255',
+            'client_institution' => 'required|string|max:20',
+            'client_institution' => 'required|string|max:20',
+
+            'manuscript_path' => 'required|file|mimes:doc,docx|max:2048',
             'print_qunaitity'=> 'required|integer|max:20',
-            'package_id' => 'required|exists:publisher_packages,id',
-            'price_range_id' => 'required|exists:price_ranges,id',
         ]);
 
-        $carinamalayanan = PublisherPackage::findOrFail($request->input('package_id'));
-        $namalayanan = $carinamalayanan->package_name;
-
-        $carihargalayanan = PublisherPackage::findOrFail($request->input('package_id'));
-        $hargalayanan = $carihargalayanan->base_price;
-
-        $caripaketpenerbitan = PriceRange::findOrFail($request->input('price_range_id'));
-        $paketpenerbitan = $caripaketpenerbitan->price;
-
-        $carihargacetakan = PrintQuantity::findOrFail($request->input('print_quantity_id'));
-        $hargacetakan = $carihargacetakan->price_per_unit;
-
-        $rangehargacetakan = PrintQuantity::findOrFail($request->input('print_quantity_id'));
-        $rangecetakan = $rangehargacetakan->quantity;
-
-        $jumlahcetakancustomer = $request->input('print_qunaitity');
-
-        $totalPrice = 0;
-        if ($namalayanan === 'Menerbitkan buku') {
-            $totalPrice = $paketpenerbitan;
-        }elseif ($namalayanan === 'Menerbitkan buku dengan HKI') {
-            $totalPrice = $hargalayanan + $paketpenerbitan;
-        }
+        // $copyan = $request->input('print_qunaitity');
+        // $hargapercetakan = $copyan * ;
+        $laynanpublikasi = $request->input('service_type');
+        $paketbukuu = $request->input('book_size');
+        
+        $totalPrice = $laynanpublikasi + $paketbukuu;
 
         PublisherOrder::created([
             'client_name' => $request->input('name'),
@@ -67,12 +51,8 @@ class PublishOrderController extends Controller
             'client_institution' => $request->input('client_institution'),
             'manuscript_path' => $request->input('manuscript_path'),
             'print_qunaitity' => $request->input('print_qunaitity'),
-            'package_id' => $request->input('package_id'),
-            'price_range_id' => $request->input('price_range_id'),
-            'print_quantity_id' => $request->input('print_quantity_id'),
-            'book_hki_price_id' => $request->input('book_hki_price_id'),
-            'total_price' => $totalPrice, 
-            'status' => 'Pending', 
+            'total_price' => $totalPrice,
+            'status' => 'Pending',
         ]);
 
         return redirect()->route('orders.index')->with('success', 'Order berhasil dibuat.');
--- resources/views/publisher.blade.php
+++ resources/views/publisher.blade.php
@@ -281,20 +281,20 @@
                         <span class="sr-only">Close modal</span>
                     </button>
                 </div>
-                <form class="p-4 md:p-5" method="POST" action="/publisher-order">
+                <form class="p-4 md:p-5" method="POST" action="/publisher-order" enctype="multipart/form-data">
                     @csrf
                     <div class="grid gap-4 mb-4 grid-cols-2">
                         <div class="col-span-2">
-                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
-                            <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Nama lengkap beserta gelar" required="">
+                            <label for=client_namee" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
+                            <input type="text" name="client_name" id="client_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Nama lengkap beserta gelar" required="">
                         </div>
                         <div class="col-span-2">
                             <label for="book_title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Judul Buku</label>
                             <input type="text" name="book_title" id="book_title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Judul Buku" required="">
                         </div>
                         <div class="col-span-2">
-                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="email@gmail.com" required="">
+                            <label for="client_emailemail" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                            <input type="email" name="client_email" id="client_email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="email@gmail.com" required="">
                         </div>
 
                         <div class="relative col-span-2">
@@ -305,28 +305,28 @@
                                         <path d="M18 13.446a3.02 3.02 0 0 0-.946-1.985l-1.4-1.4a3.054 3.054 0 0 0-4.218 0l-.7.7a.983.983 0 0 1-1.39 0l-2.1-2.1a.983.983 0 0 1 0-1.389l.7-.7a2.98 2.98 0 0 0 0-4.217l-1.4-1.4a2.824 2.824 0 0 0-4.218 0c-3.619 3.619-3 8.229 1.752 12.979C6.785 16.639 9.45 18 11.912 18a7.175 7.175 0 0 0 5.139-2.325A2.9 2.9 0 0 0 18 13.446Z"/>
                                     </svg>
                                 </div>
-                                <input type="text" name="phone" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0812345678" required>
+                                <input type="text" name="client_phone" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0812345678" required>
                             </div>
                         </div>
                         
                         <div class="col-span-2">
                             <label for="date_of_birth" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tanggal Lahir</label>
-                            <input type="date" name="date_of_birth" id="date_of_birth" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+                            <input type="date" name="client_birthdate" id="date_of_birth" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
                         </div>
                         <div class="col-span-2">
                             <label for="gender" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jenis Kelamin</label>
-                            <select name="gender" id="gender" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white">
+                            <select name="client_gender" id="gender" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white">
                                 <option value="Pria">Pria</option>
                                 <option value="Wanita">Wanita</option>
                             </select>
                         </div>
                         <div class="col-span-2">
                             <label for="job_title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Pekerjaan</label>
-                            <input type="text" name="job_title" id="job_title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Pekerjaan" required>
+                            <input type="text" name="client_job_title" id="job_title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Pekerjaan" required>
                         </div>
                         <div class="col-span-2">
                             <label for="institution" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Institusi</label>
-                            <input type="text" name="institution" id="institution" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Institusi" required>
+                            <input type="text" name="client_institution" id="institution" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Institusi" required>
                         </div>
                         <div class="col-span-2">
                             <label for="service_type" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Layanan</label>
@@ -342,17 +342,18 @@
                                 @foreach ($paketbukuu as $paket)
                                     <option value={{ $paket->page_rage }}>{{ $paket->page_rage }}</option>
                                 @endforeach
-
                             </select>
                         </div>
                         <div class="col-span-2">
                             <label for="print_quantity" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jumlah Cetakan</label>
-                            <input type="number" name="print_quantity" id="print_quantity" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Number of copies" required="">
+                            <input type="number" name="print_qunaitity" id="print_quantity" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Number of copies" required="">
                         </div>
+
                         <div class="col-span-2">
-                            <label for="address" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Alamat</label>
-                            <textarea name="address" id="address" rows="3" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Your address"></textarea>
+                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload file</label>
+                            <input name="manuscript_path" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file">
                         </div>
+                        
                     </div>
                     <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Kirim
