4470e5bbde65970fd9e89ded1cb361f9e3b5bf9e
Fadhel Akbar <fadhelakbarsallang@gmail.com>
2025-01-06 21:48:35 +0800

    update form paket publikasi

--- resources/views/publisher.blade.php
+++ resources/views/publisher.blade.php
@@ -340,7 +340,7 @@
                             <label for="book_size" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ukuran Buku</label>
                             <select name="book_size" id="book_size" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white">
                                 @foreach ($paketbukuu as $paket)
-                                <option value="100-250 halaman">{{ $paket->page_rage }}</option>
+                                    <option value={{ $paket->page_rage }}>{{ $paket->price }}</option>
                                 @endforeach
 
                             </select>
