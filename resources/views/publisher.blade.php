5fdbde45dcf9bd54d2acd2c7663980db07432c63
Fadhel Akbar <fadhelakbarsallang@gmail.com>
2025-01-12 02:43:50 +0800

    fixing bug belum bisa menambahkan otrder turnitin baru

--- resources/views/publisher.blade.php
+++ resources/views/publisher.blade.php
@@ -282,12 +282,12 @@
                 @csrf
                 <div class="grid gap-4 mb-4 grid-cols-2">
                     <div class="col-span-2">
-                        <label for=client_namee" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
+                        <label for="client_namee" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
                         <input type="text" name="client_name" id="client_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Nama lengkap beserta gelar" required="">
                     </div>
                     <div class="col-span-2">
-                        <label for="book_title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama File</label>
-                        <input type="text" name="document_title" id="book_title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Contoh bab 1 proposal" required="">
+                        <label for="document_title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama File</label>
+                        <input type="text" name="document_title" id="document_title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Contoh bab 1 proposal" required="">
                     </div>
                     <div class="col-span-2">
                         <label for="client_email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
