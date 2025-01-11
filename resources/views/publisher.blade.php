9d2bc26baec368a056160c8e2d75b9b572b3a8dc
Fadhel Akbar <fadhelakbarsallang@gmail.com>
2025-01-12 01:57:01 +0800

    fixing button close turnitin order modal

--- resources/views/publisher.blade.php
+++ resources/views/publisher.blade.php
@@ -271,7 +271,7 @@
                 <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
                     Form Order Turnitinr
                 </h3>
-                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="crud-modal">
+                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="turnitin-modal">
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                     </svg>
@@ -286,8 +286,8 @@
                         <input type="text" name="client_name" id="client_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Nama lengkap beserta gelar" required="">
                     </div>
                     <div class="col-span-2">
-                        <label for="book_title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Judul Buku</label>
-                        <input type="text" name="document_title" id="book_title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Judul Buku" required="">
+                        <label for="book_title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama File</label>
+                        <input type="text" name="document_title" id="book_title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Contoh bab 1 proposal" required="">
                     </div>
                     <div class="col-span-2">
                         <label for="client_emailemail" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
