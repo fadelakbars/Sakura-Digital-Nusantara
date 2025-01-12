99fdd66c4fce3e1f44928ee87ebda699b6280e59
Fadhel Akbar <fadhelakbarsallang@gmail.com>
2025-01-12 23:11:25 +0800

    testing revert untuk kembalikan fungsi buat orderan publikasi buku

--- resources/views/publisher.blade.php
+++ resources/views/publisher.blade.php
@@ -10,12 +10,12 @@
             <h1 class="mb-4 mt-8 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Sakura Publisher</h1>
             <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Penerbit Nasional yang berfokus pada buku-buku akademik independen, kami profesional dalam menyebarluaskan hasil penelitian para peneliti Indonesia secara nasional dan global. Kami menawarkan kesempatan bagi penelitian dan pelaku pendidikan untuk mencapai komunitas akademik, dengan komitmen konsisten terhadap kualitas dan inovasi serta diseminasi melalui indeks portal akademik.</p>
             <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-                {{-- <button data-modal-target="crud-modal" data-modal-toggle="crud-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-                    Daftar Menerbitkan Buku
-                </button> --}}
-                <button id="defaultModalButton" data-modal-target="defaultModal" data-modal-toggle="defaultModal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+                <button data-modal-target="crud-modal" data-modal-toggle="crud-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
                     Daftar Menerbitkan Buku
                 </button>
+                {{-- <button id="defaultModalButton" data-modal-target="defaultModal" data-modal-toggle="defaultModal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+                    Daftar Menerbitkan Buku
+                </button> --}}
             </div>
         </div>
     </section>
